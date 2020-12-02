37721bc60d16cce12c5a6207e6be764a6cfb58bc
John Lees <lees.john6@gmail.com>
2020-12-02 14:51:10 +0000

    Add profiler headers

--- inst/include/dust/cuda.cuh
+++ inst/include/dust/cuda.cuh
@@ -12,8 +12,11 @@
 
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
 #include <device_launch_parameters.h>
 #include <cpp11/protect.hpp>
+
+#include <cub/device/device_select.cuh>
 
 static void HandleCUDAError(const char *file, int line,
                             cudaError_t status = cudaGetLastError()) {
@@ -23,9 +26,11 @@ static void HandleCUDAError(const char *file, int line,
 
   if (status != CUDA_SUCCESS || (status = cudaGetLastError()) != CUDA_SUCCESS) {
     if (status == cudaErrorUnknown) {
+      cudaProfilerStop();
       cpp11::stop("%s(%i) An Unknown CUDA Error Occurred :(\n",
                   file, line);
     }
+    cudaProfilerStop();
     cpp11::stop("%s(%i) CUDA Error Occurred;\n%s\n",
                 file, line, cudaGetErrorString(status));
   }

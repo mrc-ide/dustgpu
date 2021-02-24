4ed5772892719e99933b41b6edcb3e846a5046f9
John Lees <lees.john6@gmail.com>
2021-02-24 14:39:38 +0000

    Add CUDA to other distributions (except Poisson)

--- inst/include/dust/distr/uniform.hpp
+++ inst/include/dust/distr/uniform.hpp
@@ -4,14 +4,14 @@
 namespace dust {
 namespace distr {
 
-template <typename real_t>
-real_t runif(rng_state_t<real_t>& rng_state,
-             typename rng_state_t<real_t>::real_t min,
-             typename rng_state_t<real_t>::real_t max) {
+__nv_exec_check_disable__ template <typename real_t>
+HOSTDEVICE real_t runif(rng_state_t<real_t> &rng_state,
+                        typename rng_state_t<real_t>::real_t min,
+                        typename rng_state_t<real_t>::real_t max) {
   return dust::unif_rand<real_t>(rng_state) * (max - min) + min;
 }
 
-}
-}
+} // namespace distr
+} // namespace dust
 
 #endif

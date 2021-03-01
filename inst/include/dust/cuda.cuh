21b13b25eb4e9f61208cefbe498c734afdd5102e
John Lees <lees.john6@gmail.com>
2021-03-01 16:05:06 +0000

    Remove unneeded template

--- inst/include/dust/cuda.cuh
+++ inst/include/dust/cuda.cuh
@@ -72,7 +72,6 @@ DEVICE void shared_mem_cpy(cooperative_groups::thread_block& block,
 #endif
 }
 
-template <typename T>
 DEVICE void shared_mem_wait(cooperative_groups::thread_block& block) {
 #if __CUDACC_VER_MAJOR__ >= 11
   cooperative_groups::wait(block);
